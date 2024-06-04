3eddeb163e3ec1d8a5fc74bdb493c0247d937173
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-04 18:46:14 +0700

    add paging result query

---
+++ EmployeeManagement.Core/Common/Paging.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace EmployeeManagement.Core.Common;
+
+[DebuggerStepThrough]
+[DebuggerDisplay("Total:{Total} Current:{Current}")]
+public class Paging<T>
+{
+    public T[] Items { get; set; }
+    public int Total { get; set; }
+    public int Current { get; set; }
+
+    public Paging(T[] items, int total, int current = 0)
+    {
+        Items = items;
+        Total = total;
+        Current = current;
+    }
+}
