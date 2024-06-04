f0082133fa62616723e00ab1c874e8b754d4ba20
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-04 18:46:34 +0700

    add exception extension

---
+++ EmployeeManagement.Core/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagement.Core.Exceptions;
+
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException(string? message) : base(message)
+    {
+    }
+
+    public EntityNotFoundException(string type, int id) : base($"Entity of type {type}:{id} could not be found")
+    {
+    }
+}
