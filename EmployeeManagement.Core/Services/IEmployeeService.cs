5a9e325d1d2cbfb7324f9816ec7134f6f7f252b4
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-04 18:49:11 +0700

    add employee service interface

---
+++ EmployeeManagement.Core/Services/IEmployeeService.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagement.Core.Common;
+using EmployeeManagement.Shared.Models;
+
+namespace EmployeeManagement.Core.Services;
+
+public interface IEmployeeService
+{
+    /// <summary>
+    /// Add employee
+    /// </summary>
+    /// <param name="employee"><see cref="EmployeeModel"/> </param>
+    /// <returns></returns>
+    Task<Result<EmployeeModel>> AddEmployeeAsync(EmployeeModel employee);
+
+    /// <summary>
+    /// Get paged of employees
+    /// </summary>
+    /// <param name="filter"><see cref="PagingRequest"/></param>
+    /// <returns></returns>
+    Task<Paging<EmployeeModel>> GetEmployeeListAsync(PagingRequest? filter = null);
+
+    /// <summary>
+    /// Get employee by id
+    /// </summary>
+    /// <param name="id">Employee's id</param>
+    /// <returns></returns>
+    Task<EmployeeModel> GetEmployeeByIdAsync(int id);
+}
+
